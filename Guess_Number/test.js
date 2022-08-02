52e474edef037330fec0acaa86d4233540b1c3eb
berk-girgin <berk.girgin@zattoo.com>
2022-08-02 23:18:33 +0200

    adding the number game

---
+++ Guess_Number/test.js
@@ -0,0 +1,71 @@
+
+const guessField = document.querySelector(".guessField");
+const guessSubmit = document.querySelector(".guessSubmit");
+const previousGuesses = document.querySelector(".previousGuesses");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+
+guessSubmit.addEventListener( "click", checkGuess );
+let randomNumber = generateRandomNumber();
+
+let guessCount = 1;
+
+
+function generateRandomNumber() {
+  //generate a random number between 1 and 100
+
+  let result = Math.floor(Math.random() * 100) + 1;
+  return result;
+}
+
+
+function checkGuess() {
+
+  const userGuess = Number(guessField.value);
+
+  if (userGuess === randomNumber) {
+    lowOrHi.textContent = "You have won, congrats!!"
+    lowOrHi.style.backgroundColor = "green"
+    resetGame();
+  } else if (guessCount == 10) {
+    gameOver();
+  }
+
+  if (userGuess > randomNumber) {
+    lowOrHi.textContent = "your number is too high"
+    lowOrHi.style.backgroundColor = "red"
+  } else if ( userGuess < randomNumber) {
+    lowOrHi.textContent = "your number is too low"
+    lowOrHi.style.backgroundColor = "red"
+  }
+
+  guessCount++;
+  previousGuesses.textContent += userGuess + " ";
+  guessField.value = "";
+  guessField.focus();
+}
+
+function gameOver(){
+  lowOrHi.textContent = "You lost :("
+  lowOrHi.style.backgroundColor = "red"
+  resetGame();
+}
+
+function resetGame(){
+  const resetButton = document.createElement("button");
+  resetButton.innerText = "Reset Button";
+  resetButton.addEventListener("click", resetGameFunction);
+  document.body.appendChild(resetButton);
+
+  function resetGameFunction(){
+    guessCount = 1;
+    previousGuesses.textContent = "Previous guesses: ";
+    lowOrHi.textContent = "";
+    guessField.focus();
+  }
+  
+
+
+}
+
+
